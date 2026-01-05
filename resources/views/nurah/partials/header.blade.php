fa90f14ee93746d16f346b74c973967958f077a4
neraj <nerajnerajlal@gmail.com>
2026-01-05 13:31:37 +0530

    nurah header

--- resources/views/nurah/partials/header.blade.php
+++ resources/views/nurah/partials/header.blade.php
@@ -39,9 +39,9 @@
     <ul class="menu-list">
         <li class="menu-item"><a href="{{ route('home') }}" class="menu-link">Home</a></li>
         <li class="menu-item"><a href="{{ route('collection') }}" class="menu-link">Shop All</a></li>
-        <li class="menu-item"><a href="#" class="menu-link">All Products</a></li>
-        <li class="menu-item"><a href="#" class="menu-link">Categories</a></li>
-        <li class="menu-item"><a href="#" class="menu-link">Combo Offers</a></li>
+        <li class="menu-item"><a href="{{ route('all-products') }}" class="menu-link">All Products</a></li>
+        <li class="menu-item"><a href="{{ route('collection') }}" class="menu-link">Categories</a></li>
+        <li class="menu-item"><a href="{{ route('combos') }}" class="menu-link">Combo Offers</a></li>
         <li class="menu-item"><a href="{{ route('about') }}" class="menu-link">About Us</a></li>
         <li class="menu-item"><a href="{{ route('contact') }}" class="menu-link">Contact</a></li>
     </ul>
